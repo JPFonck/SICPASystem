7476a960274f90e1ea8fc06f3f7f4438f4bad58c
Julian Mateo Puga <julianmateo.puga@udla.edu.ec>
2022-05-30 23:37:21 -0500

    Se agregó el controlador de Enterprises, su vista de index y Create

---
+++ Controllers/EnterprisesController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SICPASystem.Data;
+using SICPASystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SICPASystem.Controllers
+{
+    public class EnterprisesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EnterprisesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //Http Get Index
+        public IActionResult Index()
+        {
+            IEnumerable<EnterpriseModel> listEnterprises = _context.Enterprise;
+
+            return View(listEnterprises);
+        }
+
+        //Http Get Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //Http Post Create
+        [HttpPost]
+        public IActionResult Create(EnterpriseModel enterprise)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Enterprise.Add(enterprise);
+                _context.SaveChanges();
+
+                TempData["mensaje"] = "Enterprise Saved";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
